5fb25a8d47990a91188f73daf8cbd99765524f2f
Abishek Aryal <abishekaryal9558@gmail.com>
2025-05-20 13:01:35 -0400

    excercise 1

---
+++ CustomCollections/src/main/java/com/pluralsight/collection/FixedList.java
@@ -0,0 +1,27 @@
+package com.pluralsight.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+        public class FixedList<T> {
+            private final int maxSize;
+            private final List<T> items;
+
+            public FixedList(int maxSize) {
+                this.maxSize = maxSize;
+                this.items = new ArrayList<>();
+            }
+
+            public void add(T item) {
+                if (items.size() >= maxSize) {
+                    System.out.println("cannot add item: maximum reached" + maxSize);
+                    return;
+                }
+                items.add(item);
+            }
+
+            public List<T> getItems() {
+                return items;
+            }
+        }
+
